35c9739fa0e3a22a0e615e301d595aa174f9a7c0
James Connolly <james.connolly@lyit.ie>
2021-03-18 09:54:12 +0000

    Update managers.R
    Updated code

--- managers.R
+++ managers.R
@@ -1,45 +1,66 @@
- This script builds the managers dataset
-# and populates it with data
-
-# Load data from previous session
-column_names <- c("Date", "Country", "Gender", "Age", "Q1", "Q2", "Q3", "Q4", "Q5")
-
 # Enter data into vectors before constructing the data frame
-date_col <- c("2018-15-10", "2018-01-11", "2018-21-10", "2018-28-10", "2018-01-05")
+date_col <- c("10/15/18", "01/11/18", "10/21/18", "10/28/18", "05/01/18")
 country_col <- c("US", "US", "IRL", "IRL", "IRL")
 gender_col <- c("M", "F", "F", "M", "F")
-age_col <- c(32, 45, 25, 39, 99) # 99 is one of the values in the age attribute - will require recoding
+age_col <- c(32, 45, 25, 39, 99)
 q1_col <- c(5, 3, 3, 3, 2)
 q2_col <- c(4, 5, 5, 3, 2)
 q3_col <- c(5, 2, 5, 4, 1)
 q4_col <- c(5, 5, 5, NA, 2) # NA is inserted in place of the missing data for this attribute
 q5_col <- c(5, 5, 2, NA, 1)
 
-# Construct a data frame using the data from all vectors above
-managers <- data.frame(date_col, country_col, gender_col, age_col, q1_col, q2_col, q3_col, q4_col, q5_col)
+# Construct a data frame using the data from all the vectors
+managers_data <- data.frame(date_col, 
+                            country_col, 
+                            gender_col, 
+                            age_col, 
+                            q1_col, 
+                            q2_col, 
+                            q3_col, 
+                            q4_col, 
+                            q5_col)
 
-# Add column names to data frame using column_names vector
-colnames(managers) <- column_names
+managers_data
 
-# Recode the incorrect 'age' data to NA
-managers$Age[managers$Age == 99] <- NA
+column_names <- c("Date", 
+                  "Country", 
+                  "Gender", 
+                  "Age", 
+                  "Q1", 
+                  "Q2", 
+                  "Q3", 
+                  "Q4", 
+                  "Q5")
 
-# Create a new attribute called AgeCat and set valuess
-# in AgeCat to the following if true:
-# <= 25 = Young
-# >= 26 & <= 44 = Middle Aged
-# >= 45 = Elderly
-# We will also recode age 'NA' to Elder
+# Add column names to the managers_data dataframe
+colnames(managers_data) <- column_names
 
-managers$AgeCat[managers$Age >= 45] <- "Elder"
-managers$AgeCat[managers$Age >= 26 & managers$Age <= 44] <- "Middle Aged"
-managers$AgeCat[managers$Age <= 25] <- "Young"
-managers$AgeCat[is.na(managers$Age)] <- "Elder"
+managers_data
 
-# Recode AgeCat so that is ordinal and factored with the
-# order Young, Middle aged, Elder
-# We'll srore the ordinal factored data in variable 'AgeCat'
-AgeCat <- factor(managers$AgeCat, order = TRUE, levels = c("Young", "Middle Aged", "Elder"))
+# Recode the incorrect 'age' to NA
+managers_data$Age[managers_data$Age == 99] <- NA
+managers_data
 
-# Replace managers's AgeCat attribute with newly ordinal foctored data
-managers$AgeCat <- AgeCat
+# 2 options to create a new variable
+# 1 - create a new vector and store the logical check in it
+# 2 - create the new var when doing the logical check
+
+managers_data$AgeCat[managers_data$Age >= 45] <- "Elder"
+managers_data$AgeCat[managers_data$Age >= 26 & managers_data$Age <= 44] <- "Middle Aged"
+managers_data$AgeCat[managers_data$Age <= 25] <- "Young"
+managers_data$AgeCat[is.na(managers_data$Age)] <- "Elder"
+managers_data
+
+# recode age_cat so that it is ordinal and factored
+# with the order young, middle aged, elder
+age_cat <- factor(managers_data$AgeCat, order = TRUE, levels = c("Young", "Middle Aged", "Elder"))
+age_cat
+
+# replace mnager_data age_cat variable with
+# the factored variable
+
+managers_data$AgeCat <-age_cat
+managers_data
+
+# Look at the structure of the data frame
+str(managers_data)
